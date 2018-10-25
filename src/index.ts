7d128a58713d34970faa5a2ac8335440bf7f391e
Lampi84 <patric.lambrecht@gmail.com>
2018-10-25 23:44:23 +0200

    First version

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { TranslateLoader } from '@ngx-translate/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+export class TranslatePoHttpLoader implements TranslateLoader {
+  /**
+   * Translation domain
+   */
+
+  constructor(
+    protected _http: HttpClient,
+    protected _path: string = './assets/i18n',
+    protected _suffix: string = '.js'
+  ) {}
+
+  /**
+   * Gets the translations from file
+   * @param lang
+   * @returns {any}
+   */
+  public getTranslation(lang: string): Observable<any> {
+    return this._http
+      .jsonp(`${this._path}/${lang}${this._suffix}`, 'callback')
+      .pipe(map((contents: string) => contents));
+  }
+}
